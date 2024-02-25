f335debe5980cc5dd0f188e0e2e2f1fd6b4b65f1
egorReka <e.reka55@yandex.ru>
2024-02-25 12:38:16 +0300

    fix: lint

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -1,12 +1,12 @@
 /* в этот файл добавляет скрипты*/
 
-const slider = document.querySelector('.filter-price__slider');
+// const slider = document.querySelector('.filter-price__slider');
 
-noUiSlider.create(slider, {
-  start: [20, 80],
-  connect: true,
-  range: {
-    'min': 0,
-    'max': 100
-  }
-});
+// noUiSlider.create(slider, {
+//   start: [20, 80],
+//   connect: true,
+//   range: {
+//     'min': 0,
+//     'max': 100
+//   }
+// });
