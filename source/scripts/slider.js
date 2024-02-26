d37a4125d6aa10206f6d919f9fd4941909783c6e
egorReka <e.reka55@yandex.ru>
2024-02-26 23:34:07 +0300

    fix: minor edits

--- source/scripts/slider.js
+++ source/scripts/slider.js
@@ -1,8 +1,7 @@
-const slider = document.querySelector('.slider')
+const slider = document.querySelector('.slider');
 const sliderList = slider.querySelector('.slider__list');
 const sliderItems = slider.querySelectorAll('.slider__item');
 const sliderPaginations = slider.querySelectorAll('.slider-pagination__item');
-
 const buttonLeft = slider.querySelector('.slider__button--left');
 const buttonRight = slider.querySelector('.slider__button--right');
 
@@ -27,23 +26,27 @@ const toggleButton = (pressedButton) => {
   }
 };
 
+const changPagination = () => {
+  slider.querySelector('.slider-pagination__item--current').classList.remove('slider-pagination__item--current');
+  sliderPaginations[countCurentSlide].classList.add('slider-pagination__item--current');
+};
+
 const sliderInit = () => {
   buttonLeft.addEventListener('click', function () {
     sliderList.style.transform = `translateX(${100 + countPercentStep}%)`;
     countPercentStep += 100;
     countCurentSlide -= 1;
-    slider.querySelector('.slider-pagination__item--current').classList.remove('slider-pagination__item--current');
-    sliderPaginations[countCurentSlide].classList.add('slider-pagination__item--current');
+
+    changPagination();
     toggleButton(this);
   });
 
   buttonRight.addEventListener('click', function () {
-
     sliderList.style.transform = `translateX(${-100 + countPercentStep}%)`;
     countPercentStep -= 100;
     countCurentSlide += 1;
-    slider.querySelector('.slider-pagination__item--current').classList.remove('slider-pagination__item--current');
-    sliderPaginations[countCurentSlide].classList.add('slider-pagination__item--current');
+
+    changPagination();
     toggleButton(this);
   });
 };
